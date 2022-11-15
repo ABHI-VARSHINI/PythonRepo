329ba2a09eb88006bf74757d915fc10bffc2e0a9
ABHI-VARSHINI <35648305+ABHI-VARSHINI@users.noreply.github.com>
2022-11-15 20:25:02 +0530

    Create Python_Ifelse.py

---
+++ Python_Ifelse.py
@@ -0,0 +1,16 @@
+#if else
+
+x=5
+y=5
+
+if x==y:
+    print("Yes, X is equal to Y")
+else:
+   print("No, X is not equal to Y") 
+
+#without using else block
+a=10
+
+if a==10:
+    print("Yes, A is equal to 10 !!")
+print("Bye !!")
