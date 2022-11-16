284f5209ce4edb955c458a857b32717c509d7ea4
ABHI-VARSHINI <35648305+ABHI-VARSHINI@users.noreply.github.com>
2022-11-16 14:49:19 +0530

    Create Python_whileloop.py

---
+++ Python_whileloop.py
@@ -0,0 +1,13 @@
+#Syntax
+#while exp:
+#   Statements
+
+#Write a program to print the table of 9 using while loop
+i=1
+while i<=10:
+    ans=i*9
+    print("9 * ",i,"= ", ans)
+    i+=1
+
+#what will happens if i is not inceremented
+# system gets crashed until no space left
