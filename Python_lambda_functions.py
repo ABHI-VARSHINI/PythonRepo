fee05f1377ef2af44f5df27219478791c3f026b9
ABHI-VARSHINI <35648305+ABHI-VARSHINI@users.noreply.github.com>
2022-11-19 16:09:15 +0530

    Update Python_lambda_functions.py

--- Python_lambda_functions.py
+++ Python_lambda_functions.py
@@ -27,3 +27,47 @@ print(lambda_maxoftwo(6,7))
 
 #How to work with map(), filter() and reduce()
 
+#Implement map() function
+list1 = [1,2,3,4,5,6,7,8,9]
+
+#Result
+#result = [1,4,9,16,25,36,49,64,81]
+
+square_num = lambda sqr : sqr*sqr
+square_list = map(square_num, list1)
+
+print(list(square_list))
+
+#Add sequential respective elements in two given list
+list_a = [1,2,3,4,3]
+list_b = [5,4,5,2,1]
+
+#Result= [6,6,8,6,4]
+add_sequential = lambda a,b : a+b
+add_lists = map(add_sequential,list_a,list_b)
+
+print("Addition of two lists: ",list(add_lists))
+
+#Implement reduce() function
+import functools #reduce() function is present inside reduce() function
+
+list_x = [1,2,3,4,5]
+
+add_two_nums = lambda x,y : x+y
+result = functools.reduce(add_two_nums, list_x)
+print(result)
+
+#Multiply two nums
+list_x = [1,2,3,4,5]
+
+add_two_nums = lambda x,y : x*y
+result = functools.reduce(add_two_nums, list_x)
+print(result)
+
+#How to use filter()
+#Create list to filter odd elements
+seq = [1,2,5,6,7,9,13]
+
+filter_odd = lambda x : x%2!=0
+result = filter(filter_odd, seq)
+print(list(result))
